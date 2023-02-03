58894d0a95346b87e7799dc4c767a61f1e253c9f
laura <l.paillet@ecole-ipssi.net>
2023-02-03 16:09:52 +0100

    modal génial

--- src/Components/js/Card.js
+++ src/Components/js/Card.js
@@ -3,55 +3,25 @@ import '../css/Card.css';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
-
-function Marvel(props) {
-  // console.log(props.onHide);
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-      show={props.show}
-      onHide={props.onHide}
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-
-    
-  );
-}
-
 function Card({ data }) {
   const [modalShow, setModalShow] = useState(false);
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
       <div className='cardContainer'>
+        
           {
             ((data && data.length)) ? (
           data.map(item => {
                     return (
                     <ul className='cards'>
                         <li className='oneCard' key={item.id}
-                          onClick={() => setModalShow(true)}>
+                          onClick={handleShow}>
                           {console.log(modalShow)}
-                          <Marvel  show={modalShow}
+                          <Modal  show={modalShow}
                             onHide={() => setModalShow(false)}/>
 
                 
@@ -73,8 +43,27 @@ function Card({ data }) {
                 })
             ):""
           }
+          <Modal  show={show} onHide={handleClose} >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={handleClose}>Close</Button>
+      </Modal.Footer>
+    </Modal>
           
     </div>
+    
   )
 }
 
